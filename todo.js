cb8adf1b3c9f8fd610227e63e1f0368c2235d298
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2020-01-10 17:25:09 -0300

    :construction: changing to-do list for a dynamic data structure

---
+++ todo.js
@@ -0,0 +1,9 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var toDos = [
+    'Tarefa 1',
+    'Tarefa 2',
+    'Tarefa 3'
+];
